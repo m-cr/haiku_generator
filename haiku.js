8bc1eb343d7d2b92583d46ecb8e6631c6de96d3d
Michael Crowley <miketcrowley@gmail.com>
2016-06-30 12:28:21 -0400

    Random Structure Haiku Generator

--- haiku.js
+++ haiku.js
@@ -8,40 +8,24 @@ function readCmudictFile(file){
 var syllableArray = [0,[],[],[],[],[],[],[]];
 
 function formatData(data){    
-   var lines = data.toString().split("\n"),
+  var lines = data.toString().split("\n"),
        lineSplit
    
-   lines.forEach(function(line){    
+  lines.forEach(function(line){    
     lineSplit = line.split("  ");
     var word = lineSplit[0];
     var phoneme = lineSplit[1];    
-    
-    if(phoneme != undefined){
-    	if(phoneme.match(/\d/)){
-    		if(phoneme.match(/\d/g).length === 1){
-    			syllableArray[1].push(word);
-    		}
-    		if(phoneme.match(/\d/g).length === 2){
-    			syllableArray[2].push(word);
-    		}
-    		if(phoneme.match(/\d/g).length === 3){
-    			syllableArray[3].push(word);
-    		}
-    		if(phoneme.match(/\d/g).length === 4){
-    			syllableArray[4].push(word);
-    		}
-    		if(phoneme.match(/\d/g).length === 5){
-    			syllableArray[5].push(word);
-    		}
-    		if(phoneme.match(/\d/g).length === 6){
-    			syllableArray[6].push(word);
-    		}
-    		if(phoneme.match(/\d/g).length === 7){
-    			syllableArray[7].push(word);
-    		}
-    	}
-	}	
-  	});   
+      
+    for (var i = 0; i < syllableArray.length; i++) {
+      if(phoneme != undefined){
+        if(phoneme.match(/\d/)){
+          if(phoneme.match(/\d/g).length === i){
+            syllableArray[i].push(word);
+          }
+        }
+      }
+    }
+  });   
 }
 
 formatData(cmudictFile);
--- haiku_generator.js
+++ haiku_generator.js
@@ -1,9 +1,18 @@
 var haiku = require('./haiku');
 
-console.log(haiku.createHaiku( [[5],[7],[5]], haiku.syllableArray) );
+var structure = function(){
+	var strucArr = [[],[],[]];
+	strucArr[0].push(Math.floor(Math.random()*5));
+	if(strucArr[0][0] < 5){strucArr[0][1] = 5 - strucArr[0][0]};
+	strucArr[1].push(Math.floor(Math.random()*5));
+	if(strucArr[1][0] < 7){strucArr[1][1] = 7 - strucArr[1][0]};
+	strucArr[2].push(Math.floor(Math.random()*5));
+	if(strucArr[2][0] < 5){strucArr[2][1] = 5 - strucArr[2][0]};
+	return strucArr;
+}
 
-// [
-// [5],
-// [7],
-// [5]
-// ]
+haikuStructure = structure();
+
+// console.log(structure())
+
+console.log( haiku.createHaiku( haikuStructure, haiku.syllableArray) );
